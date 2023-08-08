191cbf774737083cf280450403c53073b5be4fab
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-08-08 18:51:07 +0900

    java spring framework

--- testproject/src/main/java/spring4/testproject/TestprojectApplication.java
+++ testproject/src/main/java/spring4/testproject/TestprojectApplication.java
@@ -2,9 +2,6 @@ package spring4.testproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
-import spring4.testproject.Configuration.BeanTestctx;
 
 @SpringBootApplication
 public class TestprojectApplication {
@@ -23,9 +20,5 @@ public class TestprojectApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TestprojectApplication.class, args);
-
-		// transpotationWalk.move();
-		System.out.println("ctx 객체 생성");
-		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(BeanTestctx.class);
 	}
 }
