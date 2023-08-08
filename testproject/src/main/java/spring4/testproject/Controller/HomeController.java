b67a8744ae40d02be63a6b7f886fd9a023a951e1
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-08-08 17:32:01 +0900

    java spring framework

--- testproject/src/main/java/spring4/testproject/Controller/HomeController.java
+++ testproject/src/main/java/spring4/testproject/Controller/HomeController.java
@@ -2,15 +2,13 @@ package spring4.testproject.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     public String login(Model model) {
-
-        model.addAttribute("loginKey", "loginValue");
         return "login";
     }
 
